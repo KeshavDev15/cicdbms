b91726ca78981b9b3157fcfeababb75437789b1d
Keshav Agarwal <agarwalkeshav875@gmail.com>
2025-02-07 14:01:41 +0530

    Update page.tsx
    added username and password

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -8,7 +8,9 @@ export default async function Home() {
   const user = await client.user.findFirst();
   return (
     <div>
+      Username:
       <h1>Hello {user?.username}</h1>
+      Password:
       <h1>Hello {user?.password}</h1>
     </div>
   );
